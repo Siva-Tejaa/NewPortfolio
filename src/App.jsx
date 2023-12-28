0aa9788e5f90140aa8bb33b3a0023e6740ed31e0
Siva-Tejaa <asivateja1999@gmail.com>
2023-12-28 15:36:12 +0530

    new portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import React from 'react';
+import React from "react";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="text-3xl font-bold underline">
+      New Potfolio Website Updated from Xoriant Laptop
+    </div>
+  );
+};
 
-      New Potfolio Website
-      
-      </div>
-  )
-}
-
-export default App
+export default App;
