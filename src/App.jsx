bf297ffd86543c8865e15b34f7140515d298bdda
Siva-Tejaa <asivateja1999@gmail.com>
2024-01-15 23:01:01 +0530

    header and breakpoints added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header/Header";
 
 const App = () => {
   return (
-    <div className="text-3xl font-bold underline">
-      New Potfolio Website Updated from Xoriant Laptop
-    </div>
+    <main>
+      <Header />
+    </main>
   );
 };
 
